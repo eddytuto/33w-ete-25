f1787d31628f56ddc889457417e05a6faaf614e3
Eddy Martin <eddypmartin@gmail.com>
2025-06-10 10:01:18 -0400

    c7 adapter l'affichage de la carte avec flexbox

--- front-page.php
+++ front-page.php
@@ -20,23 +20,30 @@
   </div>
 </section>
 <section class="populaire">
-  <?php if (have_posts()) {
-    while (have_posts()) {
-      /* affiche l'image « mise en avant » miniature */
-      the_post();
-      the_post_thumbnail('thumbnail');
-  ?>
-      <h1><?php
-          /* affiche le titre pricipal du « post » */
-          the_title(); ?></h1>
+  <div class="conteneur global">
+    <?php if (have_posts()) {
+      while (have_posts()) {
+        /* affiche l'image « mise en avant » miniature */
+        the_post();
 
-  <?php
-      /* cette fontion permet d'afficher l'ensemble du contenu du post (article ou page)*/
-      // the_content();
-      $lien = "<a href=" . get_permalink() . ">Suite</a>";
-      echo wp_trim_words(get_the_excerpt(), 10, $lien);
-      //wp_trim_words()
-    }
-  } ?>
+    ?>
+        <article class="conteneur__carte">
+          <?php the_post_thumbnail('thumbnail'); ?>
+
+
+          <h2><?php
+              /* affiche le titre pricipal du « post » */
+              the_title(); ?></h2>
+          <p><?php
+              /* cette fontion permet d'afficher l'ensemble du contenu du post (article ou page)*/
+              // the_content();
+              $lien = "<a href=" . get_permalink() . ">Suite</a>";
+              echo wp_trim_words(get_the_excerpt(), 10, $lien);
+              //wp_trim_words()
+              ?></p>
+        </article>
+    <?php }
+    } ?>
+  </div>
 </section>
 <?php get_footer();
